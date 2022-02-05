2fbd8f335574c444f9c73938c74d03089b678532
Denis Krupkin <Denis_Krupkin@epam.com>
2022-02-05 16:56:05 +0300

    Corrected version of home task for strings module.

--- Strings/strings_home_task.py
+++ Strings/strings_home_task.py
@@ -2,10 +2,9 @@
 Home task for Module 3 - Strings
 
 """
-import re
 
 # Creation of variable as required at home task
-home_task_variable = """homEwork:
+corrupted_text = """homEwork:
 
   tHis iz your homeWork, copy these Text to variable.
 
@@ -21,47 +20,36 @@ home_task_variable = """homEwork:
 
   last iz TO calculate nuMber OF Whitespace characteRS in this Text. caREFULL, not only Spaces, but ALL whitespaces. I got 87."""
 
-# Preparations to formatting
-home_task_variable_list = [item for item in home_task_variable]  # Creation of temporary list
-temp_string_variable = home_task_variable.split('\n  ')  # Split of string variable to one dict
-strings_dict = {}
+# Cleaning of text and adding it to the one list
+formatted_text = []  # Creatre an empty list for formatted text
+for paragraph in corrupted_text.split('.\n\n\n\n  '):  # Split paragraphs by \n\n\n\n
+    temporary_list_for_paragraph = []  # Initialize separate list for one paragraph for further operations
+    temporary_string_for_paragraph = ''  # Initialize temporary string for one paragraph
+    for sentences in paragraph.split('.'):  # Split sentences inside of paragraph by '.'
+        sentences = sentences.strip().lower().replace(' iz ', ' is ').capitalize()  # Polish the sentence, fix " iz "
+        temporary_list_for_paragraph.append(sentences)  # Adding to temp list
+        temporary_string_for_paragraph = '. '.join(temporary_list_for_paragraph)  # Joint to temp string
+    formatted_text.append(temporary_string_for_paragraph + '. \n\n\n\n')  # Adding to final result with line breaks
 
-# Replace "IZ" with "is" and make sentences lowercase and Capitalize
-for_is = ' is '  # Temporary variable for store 'is'
-for index, item in enumerate(temp_string_variable):  # Enumerate through dictionary
-    strings_dict[index] = item  # Create mapping
-    for key, value in strings_dict.items():
-        strings_dict[key] = strings_dict[key].lower()  # Make all symbols lowercase
-        strings_dict[key] = re.sub(r' iz ', for_is, strings_dict[key])  # Replace "iz" with 'is'
-        strings_dict[key] = strings_dict[key].capitalize()  # Capitalize first letters in each value
-for key, value in strings_dict.items():  # Attempt to capitalize inner sentences in each value
-    number_of_dot = value.find('. ')  # Find one occurrence of '. ' in each value
-    if number_of_dot != -1:  # If found then capitalize by indexes
-        value = value[:number_of_dot] + '. ' + value[number_of_dot + 2].capitalize() + value[number_of_dot + 3:]
-        strings_dict[key] = value  # Replace value with capitalized
+# Generation of sentence from the latest words in each paragraph
+list_for_sentence = []  # Initialize of new list for sentence building
+final_sentence = ''  # Initialize of empty string for final sentence
+string_for_sentence_building = (''.join(formatted_text))  # Populate temporary string with all words from formatted text
+for _ in string_for_sentence_building.split(' \n\n\n\n'):  # Split with removing whitespaces
+    list_for_sentence = [
+        word for word in string_for_sentence_building.split(' ') if word.endswith('.')]  # Making raw sentence in list
+final_sentence = ' '.join(list_for_sentence).replace('.', '').capitalize().strip()  # Polish and add to result string
 
-# Adding of new sentence to 2 paragraph
-sentence = ''  # Empty variable for sentence
-temp_string = ''  # Empty temp string variable
-for key, value in strings_dict.items():  # For each value in dictionary
-    value = value.replace('\n', '')  # Replace \n with nothing
-    temp_string += "".join(value)  # Split all words to one temp string
-sentence += ' '.join(re.findall(r'[a-zA-Z0-9_]+\.', temp_string))  # Find all word that ends with '.'
-sentence = (sentence.replace('.', '') + ".").capitalize()  # Create one sentence with formatting
-strings_dict[2] = strings_dict[2].replace('\n', '')  # Temporary removing of \n
-strings_dict[2] = strings_dict[2] + ' ' + sentence + '\n\n'  # Adding sentence and \n\n after it for restore initial
+# Adding generated sentence to the list with cleaned text
+formatted_text[1] = formatted_text[1].replace('. \n\n\n\n', '. ') + final_sentence + '. \n\n\n\n'
 
-# Creation of final string
-formatted_string = ''  # Variable for final string
-for key, value in strings_dict.items():
-    formatted_string += value + '\n\n '  # Adding \n\n removed before
-formatted_string = formatted_string.replace(' i got 87.', ' I got 87.')  # Hard code to capitalize one last sentence
-print(formatted_string)  # Printing of final string
+# Finalizing of text
+corrected_text = ''  # # Initialize of empty string for corrected text
+for _ in formatted_text:
+    corrected_text = ' '.join(formatted_text)  # Adding list values to string
+corrected_text = corrected_text.removesuffix(' . \n\n\n\n')  # Removing of '. \n\n\n\n' at the end of final string
+print(f'Corrected_text : {corrected_text}')  # Printing of corrected text
 
 # Count of whitespaces
-set_of_whitespaces = ('\n', '\t', ' ', '\r', '\f', '\v')  # Creation of tuple with whitespaces
-count_of_whitespaces = 0  # Initialize variable for count of whitespaces
-for symbol in home_task_variable:  # For each symbol in initial string
-    if symbol in set_of_whitespaces:  # If symbol exists in list
-        count_of_whitespaces += 1  # Increase number of whitespaces
-print(f'Count of whitespaces: {count_of_whitespaces}')  # Print number of whitespaces
+count_of_whitespaces = corrected_text.count(' ') + corrected_text.count('\n')  # Calculation of whitespaces via count
+print(f'Count of whitespaces: {count_of_whitespaces}')
