cbc9aa168c595300179a782ac7b032d652784335
Denis Krupkin <Denis_Krupkin@epam.com>
2022-02-08 14:28:04 +0300

    Changes in functions names and variables names in collection file. Functions docstrings were added. Strings file added.

--- Functions/functions_collections_home_task_edited.py
+++ Functions/functions_collections_home_task_edited.py
@@ -21,61 +21,87 @@ import random
 import string
 
 
-# Function for generation of single dictionary with defined ranges of random keys and values
-def generate_dictionary_with_random_keys_and_values(
-        set_of_keys,
-        start_of_value, end_of_value,
-        start_of_total_count, end_of_total_count,
-):
-    final_dictionary = {random.choice(set_of_keys): random.randint(start_of_value, end_of_value)
-                        for _ in range(random.randint(start_of_total_count, end_of_total_count))}
-    return final_dictionary
+def generate_random_keys_values_dict(keys, min_value, max_value, min_count, max_count):
+    """
+    Function generates a dictionary with defined ranges of random keys and values.
+    @param keys: possible keys set
+    @param min_value: min possible number for value
+    @param max_value: max possible number for value
+    @param min_count: min count of values to generate
+    @param max_count: max count of values to generate
+    @return: random key to random value dict with random count of {key: value} pairs
+    """
+    return {random.choice(keys): random.randint(min_value, max_value) for _ in
+            range(random.randint(min_count, max_count))}
 
 
-# Function for generation of list with defined count of random generated dictionaries
-def create_list_with_random_dictionaries(start_of_count_of_dicts, end_of_count_of_dicts):
-    list_of_random_dicts = []
-    for _ in range(random.randint(start_of_count_of_dicts, end_of_count_of_dicts)):
-        list_of_random_dicts.append(
-            generate_dictionary_with_random_keys_and_values(string.ascii_lowercase, 0, 100, 1, 100))
-    return list_of_random_dicts
+def create_random_dictionaries_list(min_dicts_count, max_dicts_count,
+                                    keys,
+                                    min_value, max_value,
+                                    min_values_count, max_values_count):
+    """
+    Function generates list with random count of dictionaries with random set of keys and values.
+    @param min_dicts_count: min possible count of dictionaries
+    @param max_dicts_count: max possible count of dictionaries
+    @param keys: possible keys set
+    @param min_value: min possible value in dictionaries
+    @param max_value: max possible value in dictionaries
+    @param min_values_count: min possible count of keys and values in dictionaries
+    @param max_values_count: min possible count of keys and values in dictionaries
+    @return: random count of dicts with random count of pairs {key: value} in one list
+    """
+    return [generate_random_keys_values_dict(keys, min_value, max_value, min_values_count, max_values_count)
+            for _ in
+            range(random.randint(min_dicts_count, max_dicts_count))]
 
 
-# Function for generation of one dictionary with all keys and values in one place
-def create_common_dictionary_with_letter_indexes_in_keys(list_with_dictionaries):
-    common_dictionary = {}  # Define empty temporary dictionary
+def generate_all_keys_dictionary(random_dictionaries_list):
+    """
+    Function takes list with random dictionaries and generates one dictionary with all keys and values
+    with adding number of dictionary to each key.
+    @param random_dictionaries_list: list with random count of dictionaries with randon keys and values
+    @return: dictionary with all keys and values in form: {key_number: value}
+    """
+    all_keys_values_dictionary = {}  # Define empty temporary dictionary
     index = 0  # Define default index
-    for dictionary in list_with_dictionaries:  # Take list of random dictionaries
+    for dictionary in random_dictionaries_list:  # Take list of random dictionaries
         for letter, number in dictionary.items():  # Take key and value of each item
-            common_dictionary.update({f'{letter}_{index}': number})  # Add key_index: value to common dictionary
+            all_keys_values_dictionary.update({f'{letter}_{index}': number})  # Add key_index: value to dictionary
         index += 1  # Increase index
-    return common_dictionary  # Return dictionary
+    return all_keys_values_dictionary  # Return dictionary
 
 
-# Function to find the max value for each letter in dict and put it to separate dict
-def find_maximum_value_and_put_into_dictionary(set_of_keys, dictionary):
-    final_last_dict = {}  # Create final empty dictionary
-    for letter in set_of_keys:  # For defined set of possible key values
-        temp_letter_dict = {}  # Create temporary dictionary
+def find_maximum_dictionary_key_value(set_of_keys_in_dict, dictionary):
+    """
+    Function to find the max value for each single key in dict and put it to separate dict.
+    @param set_of_keys_in_dict: set of possible keys used in dictionary
+    @param dictionary: dictionary with all keys and values with added indexes in key name
+    @return: dictionary with maximum value for each unique key with number of parent dictionary in key name.
+    Adds key without number of dictionary in case if key appears only one time in all dictionaries.
+    """
+    result_dict = {}  # Create final empty dictionary
+    for key_symbol in set_of_keys_in_dict:  # For defined set of possible key values
+        same_symbol_dict = {}  # Create dictionary for store all values for separate symbol
         for _, combination in enumerate(dictionary.items()):  # For all combinations of key: value
-            if combination[0][0] == letter:  # Compare first letter in key with current letter, if matches:
+            if combination[0][0] == key_symbol:  # Compare first symbol in key with current letter, if matches:
                 dict_key, dict_value = combination  # Separate tuple to separate key and value
-                temp_letter_dict.update({dict_key: dict_value})  # Add new pair to dictionary
-        if len(temp_letter_dict) > 1:  # Check if length of temp dictionary more than 1
-            max_key = max(temp_letter_dict, key=temp_letter_dict.get)  # Find key with max value
-            final_last_dict.update({max_key: temp_letter_dict[max_key]})  # Add to final dictionary
-        elif len(temp_letter_dict) == 1:  # If length of temporary dictionary equal to one
+                same_symbol_dict.update({dict_key: dict_value})  # Add new pair to dictionary
+        if len(same_symbol_dict) > 1:  # Check if length of temp dictionary more than 1
+            max_key = max(same_symbol_dict, key=same_symbol_dict.get)  # Find key with max value
+            result_dict.update({max_key: same_symbol_dict[max_key]})  # Add to final dictionary
+        elif len(same_symbol_dict) == 1:  # If length of temporary dictionary equal to one
             updated_key = dict_key[0]  # Take only first letter of key
-            final_last_dict.update({updated_key: temp_letter_dict[dict_key]})  # Add changed key: value to final dict
-    return final_last_dict  # Return final dictionary
+            result_dict.update({updated_key: same_symbol_dict[dict_key]})  # Add changed key: value to final dict
+    return result_dict  # Return final dictionary
 
 
-list_of_random_dictionaries = create_list_with_random_dictionaries(2, 10)  # Generate list with dicts
-print(f'List with random dicts: {list_of_random_dictionaries}')
+# Generate list with random dicts
+random_dicts_list = create_random_dictionaries_list(2, 10, string.ascii_lowercase, 0, 100, 1, 100)
+print(f'List with random dicts: {random_dicts_list}')
 
 # Generate common dictionary:
-result_dictionary = create_common_dictionary_with_letter_indexes_in_keys(list_of_random_dictionaries)
+all_keys_dictionary = generate_all_keys_dictionary(random_dicts_list)
 
 # Generate final dictionary with result
-final_result_dictionary = find_maximum_value_and_put_into_dictionary(string.ascii_lowercase, result_dictionary)
-print(f'Final result dictionary: {final_result_dictionary}')
+result_dictionary = find_maximum_dictionary_key_value(string.ascii_lowercase, all_keys_dictionary)
+print(f'Result dictionary: {result_dictionary}')
---
+++ Functions/functions_strings_home_task_edited.py
@@ -0,0 +1,117 @@
+"""
+Home task for Module 4 - Functions (based on home task for Strings module)
+"""
+
+
+def fix_misspellings(misspell, correct_text, target_string):
+    """
+    Function to fix specified misspell in specified text.
+    @param misspell: misspell to fix
+    @param correct_text: correct variant of text
+    @param target_string: string to fix misspell
+    @return: string with corrected misspell
+    """
+    return target_string.replace(f'{misspell}', f'{correct_text}')
+
+
+def normalize_paragraphs_letters_case(target_text, paragraph_splitter):
+    """
+    Function generates text with normalized letters case from target_text and paragraphs structure.
+    @param target_text: text to normalize
+    @param paragraph_splitter: paragraph splitter for target text
+    @return: text with corrected letters case and paragraphs
+    """
+    # Split text by sentences and without whitespaces at the beginning and the capitalizes it:
+    target_text = '. '.join(item.lstrip().capitalize() for item in target_text.split('. '))
+    target_text_list = []  # Create list for store normalized sentences
+    for sentence in target_text.split('.\n'):  # Split text by paragraphs
+        sentence = sentence.strip()  # Delete whitespaces
+        if sentence[0].islower() and sentence[0].isalpha():  # Check is sentence starts with lower case letter
+            sentence = sentence[0].capitalize() + sentence[1:]  # Make first letter in sentence capitalized
+        target_text_list.append(sentence)  # Add normalized sentence to list
+    target_text = f'. {paragraph_splitter}'.join(target_text_list)  # Create normalized final text with paragraphs
+    return target_text  # Normalized text with correct letters case
+
+
+def generate_last_paragraph_words_sentence(target_text, paragraph_splitter):
+    """
+    Function generates sentence from last words of each sentence in text with handling of paragraphs.
+    @param target_text: text with sentences
+    @param paragraph_splitter: paragraphs splitter for handling
+    @return: sentence from last words of all sentences
+    """
+    for _ in target_text.split(f' {paragraph_splitter}'):  # Split with removing whitespaces
+        sentence_list = [
+            word for word in target_text.split(' ') if word.endswith('.')]  # Create list with words ends with '. '
+    result_sentence = ' '.join(sentence_list).replace('.', '').capitalize().strip()  # Generate sentence
+    return result_sentence
+
+
+def add_specified_sentence(sentence, paragraph_number, target_text, paragraph_splitter):
+    """
+    Function adds sentence to specified paragraph in text and save paragraphs structure.
+    @param sentence: sentence for adding
+    @param paragraph_number: number of paragraph to add sentence
+    @param target_text: text with paragraphs
+    @param paragraph_splitter: paragraphs splitter for storing paragraphs structure
+    @return: result text with sentence
+    """
+    target_text_list = target_text.split(f'{paragraph_splitter}')  # Split text by paragraphs
+    target_text_list[paragraph_number] = \
+        target_text_list[paragraph_number].replace(f'. {paragraph_splitter}', '. ') + f'{sentence}' + f'.'
+    target_text = f'{paragraph_splitter}'.join(target_text_list)  # Adding sentence with paragraph splitter at the end
+    return target_text
+
+
+def calculate_whitespaces(target_text, *whitespaces):
+    """
+    Function calculates count of specified whitespaces in target text.
+    @param target_text: text for whitespaces calculation
+    @param whitespaces: tuple with set of whitespaces
+    @return: total count of all specified whitespaces
+    """
+    count_whitespaces = 0
+    for whitespace in whitespaces:
+        if whitespace in target_text:
+            count_whitespaces += target_text.count(whitespace)
+    return count_whitespaces
+
+
+# Creation of variable as required at home task.
+corrupted_text = """homEwork:
+
+  tHis iz your homeWork, copy these Text to variable.
+
+ 
+
+  You NEED TO normalize it fROM letter CASEs point oF View. also, create one MORE senTENCE witH LAST WoRDS of each existING SENtence and add it to the END OF this Paragraph.
+
+ 
+
+  it iZ misspeLLing here. fix“iZ” with correct “is”, but ONLY when it Iz a mistAKE.
+
+ 
+
+  last iz TO calculate nuMber OF Whitespace characteRS in this Tex. caREFULL, not only Spaces, but ALL whitespaces. I got 87."""
+
+# Generate normalized text from variable.
+normalized_text = normalize_paragraphs_letters_case(corrupted_text, '\n\n\n\n')
+
+# Generate sentence from last words of all sentences.
+generated_sentence = (generate_last_paragraph_words_sentence(normalized_text, '\n\n\n\n'))
+
+# Add sentence to target paragraph in text.
+normalized_text_sentence = add_specified_sentence(generated_sentence, 1, normalized_text, '\n\n\n\n')
+
+# Fix specified misspellings in text.
+normalized_text_sentence_fixed_misspell = fix_misspellings(' iz ', ' is ', normalized_text_sentence)
+
+# Calculation count of whitespaces in text.
+whitespaces_count = calculate_whitespaces(normalized_text_sentence_fixed_misspell, ' ', '\n', '\t')
+
+
+# Printing of completely normalized text.
+print(normalized_text_sentence_fixed_misspell)
+
+# Printing count of whitespaces.
+print(f'Count of whitespaces: {whitespaces_count}')
