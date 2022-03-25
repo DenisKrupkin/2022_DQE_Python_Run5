2f7a47d214a379a8b162a9fdf4f0496e50345e74
Denis Krupkin <Denis_Krupkin@epam.com>
2022-03-25 11:32:44 +0300

    Files home task refactored. Application class removed his methods become functions. Separate method for parsing default path from app parameter added.

--- Files/files_home_task.py
+++ Files/files_home_task.py
@@ -13,18 +13,20 @@ Expand previous Homework 5 with additional class, which allow providing records 
 """
 
 import datetime
-import sys
 import os
 import text_case_normalization
 
 SEPARATOR = '------------------------------\n\n'
-DEFAULT_PATH = sys.argv[1]
+
+
+# Main classes:
 
 
 class Publication:
     """
     Class represents publication entity.
     """
+
     def __init__(self, publication_date, publication_type, city, publication_text):
         self.publication_date = publication_date
         self.publication_type = publication_type
@@ -36,6 +38,7 @@ class UserInterface:
     """
     Class for interaction with user inputs.
     """
+
     @staticmethod
     def ask_city():
         city = input("Where are you from? (City):\n")
@@ -86,6 +89,7 @@ class News(Publication):
     """
     Class that represents the news publication type. Inherits Publication class.
     """
+
     def __init__(self, publication_date, city, publication_text, publication_type=1):
         self.news_splitter = 'News -------------------------'
         super().__init__(city, publication_type, publication_date, publication_text)
@@ -95,6 +99,7 @@ class Advertisement(Publication):
     """
     Class that represents the Advertisement publication type. Inherits Publication class.
     """
+
     def __init__(self, expiration_date, publication_text, publication_date=None, publication_type=2, city=None):
         self.advertisement_splitter = 'Private Ad -------------------'
         self.expiration_date = expiration_date
@@ -108,7 +113,7 @@ class Advertisement(Publication):
         @return: count of days from today to expiration date
         """
         try:
-            expiration_days = (datetime.date.today() - datetime.date.fromisoformat(expiration_date))*-1
+            expiration_days = (datetime.date.today() - datetime.date.fromisoformat(expiration_date)) * -1
             return expiration_days.days
         except TypeError:
             print('Expiration days calculation error.')
@@ -118,6 +123,7 @@ class Article(Publication):
     """
     Class that represents the Article publication type. Inherits Publication class.
     """
+
     def __init__(self, publication_text, author, publication_date=None, publication_type=3, city=None):
         self.author = author
         self.article_splitter = 'Article ----------------------'
@@ -216,143 +222,156 @@ class PublicationsParser:
             print(f'Incorrect or empty file path:\n"{file_path}"')
             exit(1)
 
-
-class Application(News, Advertisement, Article):
-    """
-    Class Application that inherits News, Advertisement, Article classes.
-    Used for creation of final publication scenarios and running of application.
-    """
-    def __init__(self, city='', publication_text='', publication_date=''):
-        super().__init__(publication_date, city, publication_text)
-
     @staticmethod
-    def publish_news():
+    def parse_default_path():
         """
-        Method for publishing of news scenario.
-        Method creates objects of UserInterface, News classes.
+        Method reads --default_path parameter and store the path for further using.
+        @return: default path
+        """
+        import argparse
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--default_path", help='set the default path to file with publications.')
+        arg = parser.parse_args()
+        return arg.default_path
+
+
+# Publishing functions:
+
+
+def publish_news():
+    """
+        Function for publishing of news scenario.
+        Function creates objects of UserInterface, News classes.
         Asks user to enter publication text, city.
         After writes entered data to file.
         Catches exception if target file not found.
-        """
-        try:
-            user_interface = UserInterface()
-            text_case_normalizer = text_case_normalization.TextCaseNormalizer()
-            with open('News_file.txt', 'a') as publications_file:
-                publication_text = user_interface.ask_publication_text()
-                publication_text = text_case_normalizer.normalize_text_case(publication_text, '')
-                city = user_interface.ask_city()
-                publication_date = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
-                news = News(city, publication_date, publication_text)
-                publications_file.write(f'{news.news_splitter}\n')
-                publications_file.write(f'{news.publication_text}\n')
-                publications_file.write(f'{news.city}, ')
-                publications_file.write(f'{news.publication_date}\n')
-                publications_file.write(f'{SEPARATOR}')
-        except FileNotFoundError:
-            print("Target file not found. Please check News_file.txt")
+    """
+    try:
+        user_interface = UserInterface()
+        text_case_normalizer = text_case_normalization.TextCaseNormalizer()
+        with open('News_file.txt', 'a') as publications_file:
+            publication_text = user_interface.ask_publication_text()
+            publication_text = text_case_normalizer.normalize_text_case(publication_text, '')
+            city = user_interface.ask_city()
+            publication_date = datetime.datetime.today().strftime("%Y-%m-%d %H:%M:%S")
+            news = News(city, publication_date, publication_text)
+            publications_file.write(f'{news.news_splitter}\n')
+            publications_file.write(f'{news.publication_text}\n')
+            publications_file.write(f'{news.city}, ')
+            publications_file.write(f'{news.publication_date}\n')
+            publications_file.write(f'{SEPARATOR}')
+    except FileNotFoundError:
+        print("Target file not found. Please check News_file.txt")
 
-    @staticmethod
-    def publish_advertisement():
-        """
-        Method for publishing of advertisement scenario.
-        Method creates objects of UserInterface, Advertisement classes.
+
+def publish_advertisement():
+    """
+        Function for publishing of advertisement scenario.
+        Function creates objects of UserInterface, Advertisement classes.
         Asks user to enter publication text, expiration date.
         After writes entered data to file.
         Catches exception if target file not found.
-        """
-        try:
-            user_interface = UserInterface()
-            text_case_normalizer = text_case_normalization.TextCaseNormalizer()
-            with open('News_file.txt', 'a') as publications_file:
-                publication_text = user_interface.ask_publication_text()
-                publication_text = text_case_normalizer.normalize_text_case(publication_text, '')
-                expiration_date = user_interface.ask_expiration_date()
-                advertisement = Advertisement(expiration_date, publication_text)
-                publications_file.write(f'{advertisement.advertisement_splitter}\n')
-                publications_file.write(f'{advertisement.publication_text}\n')
-                publications_file.write(f'Actual until: {expiration_date}, ')
-                publications_file.write(f'{advertisement.calculate_expiration_days(expiration_date)} days left\n')
-                publications_file.write(f'{SEPARATOR}')
-        except FileNotFoundError:
-            print("Target file not found. Please check News_file.txt")
+    """
+    try:
+        user_interface = UserInterface()
+        text_case_normalizer = text_case_normalization.TextCaseNormalizer()
+        with open('News_file.txt', 'a') as publications_file:
+            publication_text = user_interface.ask_publication_text()
+            publication_text = text_case_normalizer.normalize_text_case(publication_text, '')
+            expiration_date = user_interface.ask_expiration_date()
+            advertisement = Advertisement(expiration_date, publication_text)
+            publications_file.write(f'{advertisement.advertisement_splitter}\n')
+            publications_file.write(f'{advertisement.publication_text}\n')
+            publications_file.write(f'Actual until: {expiration_date}, ')
+            publications_file.write(f'{advertisement.calculate_expiration_days(expiration_date)} days left\n')
+            publications_file.write(f'{SEPARATOR}')
+    except FileNotFoundError:
+        print("Target file not found. Please check News_file.txt")
 
-    @staticmethod
-    def publish_article():
-        """
-        Method for publishing of article scenario.
-        Method creates objects of UserInterface, Article classes.
+
+def publish_article():
+    """
+        Function for publishing of article scenario.
+        Function creates objects of UserInterface, Article classes.
         Asks user to enter publication text, author.
         After writes entered data to file.
         Catches exception if target file not found.
-        """
-        try:
-            user_interface = UserInterface()
-            text_case_normalizer = text_case_normalization.TextCaseNormalizer()
-            with open('News_file.txt', 'a') as publications_file:
-                publication_text = user_interface.ask_publication_text()
-                publication_text = text_case_normalizer.normalize_text_case(publication_text, '')
-                author = user_interface.ask_author()
-                article = Article(publication_text, author)
-                publications_file.write(f'{article.article_splitter}\n')
-                publications_file.write(f'{article.publication_text}\n')
-                publications_file.write(f'Text author: {article.author}. ')
-                publications_file.write(f'Symbols count: {article.calculate_symbols_count(publication_text)}.\n')
-                publications_file.write(f'{SEPARATOR}')
-        except FileNotFoundError:
-            print("Target file not found. Please check News_file.txt")
+    """
+    try:
+        user_interface = UserInterface()
+        text_case_normalizer = text_case_normalization.TextCaseNormalizer()
+        with open('News_file.txt', 'a') as publications_file:
+            publication_text = user_interface.ask_publication_text()
+            publication_text = text_case_normalizer.normalize_text_case(publication_text, '')
+            author = user_interface.ask_author()
+            article = Article(publication_text, author)
+            publications_file.write(f'{article.article_splitter}\n')
+            publications_file.write(f'{article.publication_text}\n')
+            publications_file.write(f'Text author: {article.author}. ')
+            publications_file.write(f'Symbols count: {article.calculate_symbols_count(publication_text)}.\n')
+            publications_file.write(f'{SEPARATOR}')
+    except FileNotFoundError:
+        print("Target file not found. Please check News_file.txt")
 
-    @staticmethod
-    def start():
-        """
-        Method for application starting.
-        Method asks user to select publication type.
+
+def start():
+    """
+        Function for application starting.
+        Function asks user to select publications input format and after according to choice loads or writes
+        publications to target file.
         After publication finished asks user to choose "Yes" for continue or stop publishing answering "No".
         Catches exception if user answered something else except "Yes" or "No".
+        Takes --default_path parameter on starting to parse file with publications if user selected such input format.
         """
-        while True:
-            user_interface = UserInterface()
-            input_format = user_interface.ask_input_format()
-            if input_format == '1':
-                publication_type = user_interface.ask_publication_type()
-                if publication_type == '1':
-                    application.publish_news()
-                elif publication_type == '2':
-                    application.publish_advertisement()
-                elif publication_type == '3':
-                    application.publish_article()
-                else:
-                    print('Please enter a number between 1 and 3. Try again please.')
-                    continue
-                user_choice = input('Do you want to add more publications? Type "Yes" or "No":\n')
-                if user_choice == 'Yes':
-                    continue
-                elif user_choice == 'No':
-                    break
-                else:
-                    print('Incorrect value entered.')
-                    break
-            elif input_format == '2':
-                file_path_mode = user_interface.ask_file_path_mode()
-                if file_path_mode == '1':
-                    publications_parser = PublicationsParser()
-                    publications_parser.check_file_path(DEFAULT_PATH)
-                    publications_parser.parse_target_file(DEFAULT_PATH)
-                elif file_path_mode == '2':
-                    file_path = user_interface.ask_file_path()
-                    publications_parser = PublicationsParser()
-                    publications_parser.check_file_path(file_path)
-                    publications_parser.parse_target_file(file_path)
-                user_choice = input('Do you want to add more publications? Type "Yes" or "No":\n')
-                if user_choice == 'Yes':
-                    continue
-                elif user_choice == 'No':
-                    break
-                else:
-                    print('Incorrect value entered.')
-                    break
+    while True:
+        user_interface = UserInterface()
+        input_format = user_interface.ask_input_format()
+        if input_format == '1':
+            publication_type = user_interface.ask_publication_type()
+            if publication_type == '1':
+                publish_news()
+            elif publication_type == '2':
+                publish_advertisement()
+            elif publication_type == '3':
+                publish_article()
+            else:
+                print('Please enter a number between 1 and 3. Try again please.')
+                continue
+            user_choice = input('Do you want to add more publications? Type "Yes" or "No":\n')
+            if user_choice == 'Yes':
+                continue
+            elif user_choice == 'No':
+                break
+            else:
+                print('Incorrect value entered.')
+                break
+        elif input_format == '2':
+            file_path_mode = user_interface.ask_file_path_mode()
+            if file_path_mode == '1':
+                publications_parser = PublicationsParser()
+                try:
+                    publications_parser.check_file_path(publications_parser.parse_default_path())
+                    publications_parser.parse_target_file(publications_parser.parse_default_path())
+                except TypeError:
+                    print('Default path is not set as parameter. '
+                          'Set "--default_path <path to file>" parameter and try again.')
+                except Exception:
+                    print('Unknown error occurred. Check the --default_path parameter.')
+            elif file_path_mode == '2':
+                file_path = user_interface.ask_file_path()
+                publications_parser = PublicationsParser()
+                publications_parser.check_file_path(file_path)
+                publications_parser.parse_target_file(file_path)
+            user_choice = input('Do you want to add more publications? Type "Yes" or "No":\n')
+            if user_choice == 'Yes':
+                continue
+            elif user_choice == 'No':
+                break
+            else:
+                print('Incorrect value entered.')
+                break
 
 
 # Application starting
 if __name__ == '__main__':
-    application = Application()
-    application.start()
+    start()
